827398a72c7dd525fc254d40883e4e6fb450f648
Seongtaek Lim <eh71@dhcp-ccc-7036.eduroam.cornell.edu>
2017-03-21 16:02:40 -0400

    changed staticfiles

--- glide/settings.py
+++ glide/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = BASE_DIR # os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -157,5 +157,4 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
   os.path.join(PROJECT_ROOT, 'static'),
-  STATIC_ROOT
 )
