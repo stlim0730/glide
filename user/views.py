52ca0b856d40a65e79e2935fbb9e2dff8c2f789e
Seongtaek Lim <eh71@dhcp-ccc-7272.eduroam.cornell.edu>
2017-03-28 13:32:59 -0400

    removed port from redirect uri for testing

--- user/views.py
+++ user/views.py
@@ -6,7 +6,7 @@ from urllib.parse import urlencode
 # Create your views here.
 def login(request):
   clientId = '1bcffc14a7141019248d'
-  redirectUrl = 'https://glidecloud.herokuapp.com:8888/user/logging_in'
+  redirectUrl = 'https://glidecloud.herokuapp.com/user/logging_in'
   scope = 'user,repo'
   githubUrl = 'https://github.com/login/oauth/authorize?client_id={}&redirect_uri={}&scope={}'.format(clientId, redirectUrl, scope)
   return redirect(githubUrl)
@@ -22,7 +22,7 @@ def loggingIn(request):
   clientSecret = 'b4398ac47191a0ccaee15117080b0c82709d46ae'
   code = request.GET.get('code', '')
   # redirectUri = 'http://localhost:8888/user/logged_in'
-  redirectUri = 'https://glidecloud.herokuapp.com:8888/user/logged_in'
+  redirectUri = 'https://glidecloud.herokuapp.com/user/logged_in'
   data = [
     ('client_id', clientId),
     ('client_secret', clientSecret),
