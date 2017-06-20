5b22ddbafa6ffad24c661f1e2c35352357a3acee
Seongtaek Lim <eh71@dhcp-ccc-2433.eduroam.cornell.edu>
2017-06-20 16:33:35 -0400

    install whitenoise; run npm webpack on production

--- glide/settings.py
+++ glide/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
   'django.middleware.security.SecurityMiddleware',
+  # 'whitenoise.middleware.WhiteNoiseMiddleware',
   'django.contrib.sessions.middleware.SessionMiddleware',
   'django.middleware.common.CommonMiddleware',
   'django.middleware.csrf.CsrfViewMiddleware',
@@ -197,6 +198,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(PROJECT_ROOT, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # OAuth credentials and info
