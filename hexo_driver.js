1fd20131310d074ccf4731b6f2500d733f9705fe
Seongtaek Lim <seongtaek.lim0730@gmail.com>
2017-12-02 17:17:22 -0500

    test basic node server communication

--- hexo_driver.js
+++ hexo_driver.js
@@ -1,2 +1,15 @@
-console.log('haha');
-return 'hahaha';
+var restify = require('restify');
+const PORT = process.env.NODE_PORT || 3000;
+
+function echo(req, res, next) {
+  res.send('hello');
+  next();
+}
+
+var server = restify.createServer();
+server.get('/', echo);
+// server.head('/hello/:name', echo);
+
+server.listen(PORT, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
