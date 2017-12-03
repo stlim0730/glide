deecc681c72abf1c93d702b3665a066a1d4b3cfb
Seongtaek Lim <seongtaek.lim0730@gmail.com>
2017-12-02 21:19:12 -0500

    upgraded Django, postgres, python

--- glide/urls.py
+++ glide/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
   url(r'^user/', include('user.urls')),
   url(r'^api/', include('api.urls')),
   url(r'^$', TemplateView.as_view(template_name='index.html')),
-  url(r'^admin/', include(admin.site.urls)),
+  url(r'^admin/', admin.site.urls),
 ]
