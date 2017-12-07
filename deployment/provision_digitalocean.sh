600cbcd802504ab704883b5a1b77444dfc6f9ae5
Seongtaek Lim <seongtaek.lim0730@gmail.com>
2017-12-07 14:00:50 -0500

    makes it work on vagrant and digitalocean

--- deployment/provision_digitalocean.sh
+++ deployment/provision_digitalocean.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
+# Note: make sure you've created a user with sudo privilege
+#   adduser <username>
+#   usermod -aG sudo <username>
+
+
 # Variables
 PG_VERSION=9.5
 PROJECT_NAME=glide
-VAGRANT_USERNAME=ubuntu
+ENV_USERNAME=glideadmin
 # WS_PORT=8889
-
-
-# Script-based provision
-# export DEBIAN_FRONTEND=noninteractive
-
-
-# 
-# Create a symbolic link to the working directory
-# 
-ln -s /vagrant /$PROJECT_NAME
 
 
 # 
@@ -30,21 +25,28 @@ sudo apt-get install -y build-essential # Required for building ANYTHING on ubun
 sudo apt-get install -y git
 
 
+#
+# Clone the project repository
+#
+cd / && sudo git clone https://github.com/stlim0730/glide.git
+
+
 # 
 # Setup Python
 # 
 sudo apt-get install -y python3-pip
 sudo pip3 install --upgrade pip
 # sudo apt-get install -y python-dev python-setuptools python-imaging libssl-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
-# # sudo export PYTHONPATH=/usr/local/lib/python3.4/dist-packages:$PYTHONPATH # Don't know why it doesn't work
-# # sudo cp /$PROJECT_NAME/deployment/aliases ~/.bash_aliases
+# sudo export PYTHONPATH=/usr/local/lib/python3.4/dist-packages:$PYTHONPATH # Don't know why it doesn't work
+# sudo cp /$PROJECT_NAME/deployment/aliases ~/.bash_aliases
 
 
 # 
 # Install Nginx
 # 
 sudo apt-get install -y nginx
-sudo cp /$PROJECT_NAME/deployment/nginx_conf /etc/nginx/sites-available/$PROJECT_NAME
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo cp /$PROJECT_NAME/deployment/nginx_conf_digitalocean /etc/nginx/sites-available/$PROJECT_NAME
 sudo ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled/
 sudo pip3 install uwsgi
 
@@ -55,24 +57,28 @@ sudo pip3 install uwsgi
 sudo apt-get install -y redis-server
 
 
-# # 
-# # Setup Database
-# # 
+# 
+# Setup Database
+# 
 sudo apt-get install -y postgresql # Installs the supported version (9.5)
 # sudo apt-get install -y postgresql-contrib # I think the previous command installs contrib-9.5
 sudo apt-get install -y libpq-dev # required for psycopg2
 sudo apt-get install -y python-psycopg2
 sudo cp /$PROJECT_NAME/deployment/pg_hba.conf /etc/postgresql/$PG_VERSION/main/
 sudo service postgresql restart
-# sudo createuser -U postgres -d $VAGRANT_USERNAME
-sudo -u postgres createuser -s $VAGRANT_USERNAME
-sudo createdb -U $VAGRANT_USERNAME $PROJECT_NAME
+# sudo createuser -U postgres -d $ENV_USERNAME
+sudo -u postgres createuser -s $ENV_USERNAME
+sudo createdb -U $ENV_USERNAME $PROJECT_NAME
 
 
 # 
 # Install Python packages
 # 
 sudo pip3 install -r /$PROJECT_NAME/requirements.txt
+
+# 
+# TODO: Reconfigure Django local settings
+# 
 
 
 # 
@@ -126,6 +132,24 @@ sudo apt-get install -y npm
 
 
 # 
+# Create a swap file
+#   to make sure npm runs without being killed during the process
+# 
+# if=/dev/zero : Read from /dev/zero file. /dev/zero is a special file in that provides as many null characters to build storage file called /swapfile1.
+# of=/swapfile1 : Read from /dev/zero write storage file to /swapfile1.
+# bs=1024 : Read and write 1024 bytes at a time.
+# count=1024k : Copy 1024k blocks input blocks.
+# (1024 bytes * 1024k = 1GB)
+# 
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1024k
+sudo chown root:root /swapfile
+sudo chmod 0600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+# To check: sudo swapon -s
+
+
+# 
 # Install NodeJS Packages
 # 
 cd /$PROJECT_NAME && sudo npm install
@@ -134,10 +158,10 @@ cd /$PROJECT_NAME && sudo npm install
 # 
 # Daemonize Daphne
 # 
-cd /$PROJECT_NAME && ./daphne.sh
+cd /$PROJECT_NAME && sudo ./daphne.sh
 
 
 # 
 # Daemonize Django worker
 # 
-cd /$PROJECT_NAME && ./runworker.sh
+cd /$PROJECT_NAME && sudo ./runworker.sh
